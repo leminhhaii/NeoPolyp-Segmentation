7c32ecfa98cb9704f3860baea00c17d99e74f2b4
leminhhaii <146369913+leminhhaii@users.noreply.github.com>
2024-11-24 22:23:47 +0700

    Update infer.py

--- infer.py
+++ infer.py
@@ -1,9 +1,11 @@
 import argparse
 import torch
-from torchvision import transforms
-from PIL import Image
+import cv2
 import segmentation_models_pytorch as smp
 import numpy as np
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
 
 parser = argparse.ArgumentParser(description="Run inference on an image using a pretrained segmentation model.")
 parser.add_argument('--image_path', type=str, required=True, help="Path to the input image.")
@@ -27,35 +29,37 @@ else:
 model.to(device)
 model.eval()
 
-
-preprocess = transforms.Compose([
-    transforms.Resize((512, 512)),   
-    transforms.ToTensor(),          
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  
+val_transform = A.Compose([
+    A.Normalize(mean=(0.485, 0.456, 0.406),std=(0.229, 0.224, 0.225)),
+    ToTensorV2(),
 ])
 
-input_image = Image.open(args.image_path).convert("RGB")  
-input_tensor = preprocess(input_image).unsqueeze(0).to(device)
+input_image = cv2.imread(args.image_path)
+input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+ori_w = input_image.shape[0]
+ori_h = input_image.shape[1]
+img = cv2.resize(input_image, (256, 256))
+transformed = val_transform(image=img)
+input_img = transformed["image"]
+input_img = input_img.unsqueeze(0).to(device)
+
+color_dict= {0: (0, 0, 0),
+             1: (255, 0, 0),
+             2: (0, 255, 0)}
+def mask_to_rgb(mask, color_dict):
+    output = np.zeros((mask.shape[0], mask.shape[1], 3))
+
+    for k in color_dict.keys():
+        output[mask==k] = color_dict[k]
+
+    return np.uint8(output) 
 
 with torch.inference_mode():
-    output = model(input_tensor)
-    output = torch.argmax(output, dim=1).squeeze().cpu().numpy()
+    output_mask = model.forward(input_img).squeeze(0).cpu().numpy().transpose(1,2,0)
+mask = cv2.resize(output_mask, (ori_h, ori_w))
+mask = np.argmax(mask, axis=2)
+mask_rgb = mask_to_rgb(mask, color_dict)
+mask_rgb = cv2.cvtColor(mask_rgb, cv2.COLOR_RGB2BGR)
+cv2.imwrite("segmentation_result.png", mask_rgb) 
 
-colormap = {
-    0: (0, 0, 0),       
-    1: (0, 255, 0),     
-    2: (255, 0, 0),     
-}
-
-def apply_colormap(segmentation_output):
-    h, w = segmentation_output.shape
-    color_image = np.zeros((h, w, 3), dtype=np.uint8)
-
-    for class_id, color in colormap.items():
-        color_image[segmentation_output == class_id] = color
-
-    return Image.fromarray(color_image)
-
-segmented_color_image = apply_colormap(output)
-segmented_color_image.save("segmented_image_color.png")
-print("Segmented image with color saved as segmented_image_color.png")
+print("Done")
